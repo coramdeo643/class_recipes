2bacbd5da26dd21a3c3f8f3efc29d88907fc0ab5
coramdeo643 <coramdeo643@kakao.com>
2025-07-25 12:47:10 +0900

    feat: Implement RecipeListItem UI
    This commit implements the UI for the `RecipeListItem` component.
    
    - The component now displays an image, title, and a descriptive text.
    - The image is loaded from the `assets/images` directory and has a 16/9 aspect ratio with rounded corners.
    - The title is displayed with a font size of 20.
    - A placeholder description "Have you ever made your own [title]? Once you've tried it, you'll never go back." is shown with a grey color and font size 13.
    - Padding and SizedBox widgets are used for spacing.

--- lib/components/recipe_list_item.dart
+++ lib/components/recipe_list_item.dart
@@ -8,6 +8,37 @@ class RecipeListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AspectRatio(
+            aspectRatio: 16 / 9,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                "assets/images/$imageName.jpeg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            title,
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            "Have you ever made your own $title? "
+            "Once you've tried it, you'll never go back.",
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 13,
+            ),
+          ),
+          SizedBox(height: 10),
+        ],
+      ),
+    );
   }
 }
